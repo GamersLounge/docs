747fbd6ba65c25fc64e01c6feb53848e531c674c
Metainy <Metainy@users.noreply.github.com>
2022-11-13 17:28:11 +0000

    deploy: 5c60ce1bdd855bfb2424eb8742b74543831b066b

--- 34891f4a48e57f74f40d76f9bad4675a711d2929/assets/docs_work_pubg.md.863c255d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o,a}from"./app.0ff5bbaf.js";const f=JSON.parse('{"title":"PUBG","description":"","frontmatter":{},"headers":[],"relativePath":"docs/work/pubg.md","lastUpdated":1668349885000}'),s={name:"docs/work/pubg.md"},r=a("h1",null,"PUBG",-1),c=[r];function n(d,p,_,i,l,u){return t(),o("div",null,c)}const h=e(s,[["render",n]]);export{f as __pageData,h as default};
---
+++ 34891f4a48e57f74f40d76f9bad4675a711d2929/assets/docs_work_pubg.md.b8de60e0.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a}from"./app.ce2e57fa.js";const f=JSON.parse('{"title":"PUBG","description":"","frontmatter":{},"headers":[],"relativePath":"docs/work/pubg.md","lastUpdated":1668349885000}'),s={name:"docs/work/pubg.md"},r=a("h1",null,"PUBG",-1),c=[r];function n(d,p,_,i,l,u){return t(),o("div",null,c)}const h=e(s,[["render",n]]);export{f as __pageData,h as default};
